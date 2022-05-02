53bc218541338c8216d0e418bc861f614a035852
Santiago Velosa <santiagovelosa@gmail.com>
2022-05-02 17:51:51 -0500

    Install react-router

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,5 +1,5 @@
-class GreetingsController < ApplicationController
-  def greeting
+class V1::GreetingsController < ApplicationController
+  def index
     random = rand(Greeting.count) + 1
     @greeting = Greeting.find(random)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  root 'greetings#index'
+  namespace :v1, defaults: { format: 'json' } do
+    get 'greetings', :to 'greetings#index'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'greetings/greeting'
-  root 'greetings#greeting'
 end
